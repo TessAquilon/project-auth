f44e6687a89b2a06be42c0f7cee75a11af67fc0d
tess.aquilon <tess.aquilon@gmail.com>
2023-05-25 17:39:17 +0200

    added a register endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -22,7 +24,7 @@ app.get("/", (req, res) => {
 });
 
 ///////////////
-// Create userSchema
+// Create user schema
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -34,10 +36,45 @@ const userSchema = new mongoose.Schema({
     required: true
   },
   accessToken: {
+    // npm i crypto
     type: String,
     default: () => crypto.randomBytes(128).toString("hex")
   }
 })
+
+// Create user model
+const User = mongoose.model("User", userSchema)
+
+// Naming conventions: Sign in/sign up OR register/log in
+
+// Create register endpoint
+app.post("/register", async (req, res) => {
+  // npm i bcrypt
+  const { username, password } = req.body;
+
+  // Hash the password
+  try {
+    const salt = bcrypt.genSaltSync();
+    const newUser = await new User({
+      username: username,
+      password: bcrypt.hashSync(password, salt)
+    }).save();
+    res.status(201).json({
+      success: true,
+      response: {
+        username: newUser.username,
+        id: newUser._id,
+        accessToken: newUser.accessToken
+      }
+    })
+  } catch (e) {
+    res.status(400).json({
+      success: false,
+      response: e
+    })
+  }
+});
+
 
 
 
