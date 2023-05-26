c668f0caa14bcf25a12ed9aadcbada784bbec5eb
tess.aquilon <tess.aquilon@gmail.com>
2023-05-26 18:49:21 +0200

    added the last functionality to the frontend

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { API_URL } from "utils/urls";
 
 const Login = () => {
@@ -8,6 +9,12 @@ const Login = () => {
     const [mode, setMode] = useState("login");
     const dispatch = useDispatch();
     const accessToken = useSelector(store => store.user.accessToken);
+    const navigate = useNavigate();
+    useEffect(() => {
+        if(accessToken) {
+            navigate("/")
+        }
+    }, [accessToken]);
     const onFormSubmit = (e) => {
         e.preventDefault();
         const options = {
@@ -21,26 +28,49 @@ const Login = () => {
         .then(res => res.json())
         .then (data => {
             if(data.success) {
-                
+
+                dispatch(user.actions.setAccesstoken(data.response.accessToken))
+                dispatch(user.actions.setUsername(data.response.username))
+                dispatch(user.actions.setUserId(data.response.id))
+                dispatch(user.actions.setError(null))
+            } else {
+                dispatch(user.actions.setAccesstoken(null))
+                dispatch(user.actions.setUsername(null))
+                dispatch(user.actions.setUserId(null))
+                dispatch(user.actions.setError(data.response))
             }
         })
     }
     return(
-        <form onSubmit={onFormSubmit}>
-            <label htmlFor="username">Username</label>
-            <input 
-                type="text" 
-                id="username" 
-                value={username}
-                onChange={e => setUsername(e.target.value)} />
-            <label htmlFor="password">Password</label>
-            <input 
-                type="password" 
-                id="password"
-                value={password}
-                onChange={e => setPassword(e.target.value)} />
-            <button type="submit">Submit</button>
-        </form>
+        <>
+            <label htmlFor="register">Register</label>
+            <input
+            type="radio"
+            id="register"
+            checked={mode === register} 
+            onChange={() => setMode("register")}/>
+            <label htmlFor="login">Login</label>
+            <input
+            type="radio"
+            id="login" 
+            checked={mode === login} 
+            onChange={() => setMode("login")}/>
+            <form onSubmit={onFormSubmit}>
+                <label htmlFor="username">Username</label>
+                <input 
+                    type="text" 
+                    id="username" 
+                    value={username}
+                    onChange={e => setUsername(e.target.value)} />
+                <label htmlFor="password">Password</label>
+                <input 
+                    type="password" 
+                    id="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)} />
+                <button type="submit">Submit</button>
+            </form>
+        </>
     );
 }
 
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -2,29 +2,56 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { API_URL } from "utils/urls";
+import { useNavigate } from "react-router-dom";
+import user from "reducers/user";
 
 const Main = () => {
     const thoughtItems = useSelector((store) => store.thoughts.items);
     const dispatch = useDispatch();
-
+    const accessToken = useSelector(store => store.user.accessToken);
+    const username = useSelector(store => store.user.username);
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (!accessToken) {
+            navigate("/login")
+        }
+    }, [accessToken]);
 
     useEffect(() => {
         const options = {
             method: "GET", 
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "Authorization": accessToken
             }
         }
         fetch(API_URL("thoughts"), options)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            if (data.success) {
+                dispatch(thoughts.actions.setError(null))
+                dispatch(thoughts.actions.setItems(data.response))
+            } else {
+                dispatch(thoughts.actions.setError(response))
+                dispatch(thoughts.actions.setItems([]))
+            }
         })
     })
 
-
+    const onLogoutButtonClick = () => {
+        dispatch(user.actions.setAccesstoken(null))
+        dispatch(user.actions.setUsername(null))
+        dispatch(user.actions.setUserId(null))
+        dispatch(user.actions.setError(null))
+    }
     return(
-        <p>I am the Main component</p>
+        <>
+            <button type="button" onClick={onLogoutButtonClick}>Logout</button>
+            username ? (<h2>These are the thoughts of {username.toUpperCase()}</h2>):""
+            {thoughtItems.map(item => {
+                return(<p key={item._id}>{item.message}</p>)
+            })}
+        </>
     );
 }
 
