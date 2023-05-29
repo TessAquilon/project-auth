6dfa659ae60fd59c99b8e93fb428b913613b6675
tess.aquilon <tess.aquilon@gmail.com>
2023-05-29 16:16:01 +0200

    fixed another mistake

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import crypto from "crypto";
 import bcrypt from "bcrypt";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/project-auth"
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/projectAuth"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
